1f0b1d1bb01278ebc246d92d86efa82c3b721485
hungleung <hungleung.lee@me.com>
2020-04-01 17:16:54 +0800

    fix

--- scripts/update_feeds.sh
+++ scripts/update_feeds.sh
@@ -35,7 +35,7 @@ install_package() {
 # Example:
 # install_package mentohust https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git
 install_package openwrt-vlmcsd https://github.com/mchome/openwrt-vlmcsd.git
-install_package luci-app-vlmcsd git clone https://github.com/mchome/luci-app-vlmcsd.git
+install_package luci-app-vlmcsd https://github.com/mchome/luci-app-vlmcsd.git
 install_package openwrt-v2ray https://github.com/kuoruan/openwrt-v2ray.git
 install_package luci-app-v2ray https://github.com/kuoruan/luci-app-v2ray.git
 install_package luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git
